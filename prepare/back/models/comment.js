d3f6bdc1769b6802b1b4d2579a1324a1430bcbca
cksdnr3 <cksdnr3333@naver.com>
2021-07-01 00:07:52 +0900

    0701 nodebird

---
+++ prepare/back/models/comment.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Comment = sequelize.define('Comment', {
+        content: {}
+    }, {
+        charset: 'utf8',
+        collate: 'utf8_general_ci', // 한글 저장
+    });
+    Comment.associate = (db) => {
+        db.Comment.belongsTo(db.User);
+        db.Comment.belongsTo(db.Post);
+
+    };
+    return Comment;
+}
