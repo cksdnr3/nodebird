32a89b8a35bebbc71960b643bf4940d1b9b47170
cksdnr3 <cksdnr3333@naver.com>
2021-07-22 16:11:05 +0900

    고정 url 테스트

--- prepare/front/sagas/user.js
+++ prepare/front/sagas/user.js
@@ -109,7 +109,7 @@ function* unfollow(action) {
   }
 }
 
-const loadMyInfoAPI = () => axios.get('/user');
+const loadMyInfoAPI = () => axios.get('http://api.chanuk.shop/user');
 
 function* loadMyInfo() {
   try {
@@ -127,7 +127,7 @@ function* loadMyInfo() {
   }
 }
 
-const loadUserAPI = (data) => axios.get(`/user/${data}`);
+const loadUserAPI = (data) => axios.get(`http://api.chanuk.shop/user/${data}`);
 
 function* loadUser(action) {
   try {
