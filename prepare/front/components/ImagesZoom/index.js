20f948d6c99afafb6a47fe4440c96fb0639b2d40
cksdnr3 <cksdnr3333@naver.com>
2021-07-23 16:00:50 +0900

    lambda connect

--- prepare/front/components/ImagesZoom/index.js
+++ prepare/front/components/ImagesZoom/index.js
@@ -24,7 +24,7 @@ const ImagesZoom = ({ images, onClose }) => {
           >
             {images.map((image) => (
               <ImgWrapper key={image.src}>
-                <img src={image.src} alt={image.src} />
+                <img src={image.src.replace(/\/thumb\//, '/original/')} alt={image.src.replace(/\/thumb\//, '/original/')} />
               </ImgWrapper>
             ))}
           </Slider>
